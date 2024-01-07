e0b5ebc4264ea948c259ac0d11672bcdee730f65
Philipp Arndt <2f.mail@gmx.de>
2024-01-07 08:47:02 +0100

    debug logging

--- app/lib/checks/index.ts
+++ app/lib/checks/index.ts
@@ -1,3 +1,4 @@
+import { log } from "../logger"
 import { checkActivity } from "./activity-check"
 import { checkMean } from "./mean-check"
 import { checkRange } from "./range-check"
@@ -7,7 +8,9 @@ export const doChecks = () => {
     const checks = [checkActivity, checkMean, checkRange, checkHigh]
 
     for (const check of checks) {
+        log.debug("Running check", check.name)
         const result = check()
+        log.debug("Result", check.name, result)
         if (result) {
             return result
         }
